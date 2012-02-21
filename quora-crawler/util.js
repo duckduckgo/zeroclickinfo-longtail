513c32f78aa7e8cb140d3c0a2bdcf5fff48dac66
Dhruv <dhruvbird@gmail.com>
2012-02-20 19:36:16 -0800

    Added jQuery & util.js

---
+++ quora-crawler/util.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var fs      = require('fs');
+var qs      = require('querystring');
+var jsdom   = require('jsdom');
+var request = require('request');
+var jquery  = fs.readFileSync(require.resolve("./jquery.min.js"), 'utf-8');
+
+function loadDOM(html, cb) {
+    try {
+        jsdom.env({
+            html: html, 
+            src: [
+                jquery
+            ],
+            done: function(errors, window) {
+                cb(window.$, window, errors);
+            }
+        });
+    } catch(ex) { }
+}
+
+function exportVars(vars) {
+    // console.error('vars:', vars);
+    if (typeof vars === 'string') {
+        vars = vars.split(/\s+/);
+    }
+    for (var i = 0; i < vars.length; ++i) {
+        exports[vars[i]] = eval(vars[i]);
+    }
+}
+
+exportVars('loadDOM');
