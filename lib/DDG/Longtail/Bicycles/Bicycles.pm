684d7fc167cd129b02912064a2c8fbd0b7787333
yegg <yegg@duckduckgo.com>
2012-11-06 14:34:48 -0500

    Add Bicycles

---
+++ lib/DDG/Longtail/Bicycles/Bicycles.pm
@@ -0,0 +1,14 @@
+package DDG::Longtail::Bicycles;
+
+use DDG::Meta::Information;
+use DDG::Longtail;
+
+name 'Bicycles';
+description 'Bicycles answers';
+source "Bicycles StackExchange";
+icon_url "/i/bicycles.stackexchange.com.ico";
+topics => 'special_interest';
+categories => 'q/a';
+primary_example_queries => 'iphone app bikers';
+secondary_example_queries => 'choose bicycle seat', 'derailleur cage lengths';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/Bicycles/';
