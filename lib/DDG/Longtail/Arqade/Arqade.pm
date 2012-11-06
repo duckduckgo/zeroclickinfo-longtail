a3cdf814ab7735f3014d53e46c4d3eb43767f02b
yegg <yegg@duckduckgo.com>
2012-11-06 14:05:53 -0500

    Add Arqade (gaming.stackexchange.com)

---
+++ lib/DDG/Longtail/Arqade/Arqade.pm
@@ -0,0 +1,14 @@
+package DDG::Longtail::Arqade;
+
+use DDG::Meta::Information;
+use DDG::Longtail;
+
+name 'Arqade';
+description 'Gaming';
+source "Gaming StackExchange";
+icon_url "/i/gaming.stackexchange.com.ico";
+topics => 'gaming';
+categories => 'q/a';
+primary_example_queries => 'avoid being detected left 4 dead';
+secondary_example_queries => 'defcon strategies', 'golden guns gears of war';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/Arqade/';
