cb1319f87b4048aff6066534e26178fe5ae117fc
yegg <yegg@duckduckgo.com>
2012-11-06 15:15:47 -0500

    add electronics (was unable to get it to work)

---
+++ lib/DDG/Longtail/ElectricalEngineering/ElectricalEngineering.pm
@@ -0,0 +1,14 @@
+package DDG::Longtail::ElectricalEngineering;
+
+use DDG::Meta::Information;
+use DDG::Longtail;
+
+name 'ElectricalEngineering';
+description 'Electrical Engineering answers';
+source "Electrical Engineering StackExchange";
+icon_url "/i/electronics.stackexchange.com.ico";
+topics => 'special_interest';
+categories => 'q/a';
+primary_example_queries => '';
+secondary_example_queries => '', '';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/ElectricalEngineering/';
