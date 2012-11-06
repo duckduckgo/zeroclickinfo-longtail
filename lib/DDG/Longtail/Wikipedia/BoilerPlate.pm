18c4db294d3878a1c56a18ac87ba47c0e020f0ea
yegg <yegg@duckduckgo.com>
2012-11-06 13:49:55 -0500

    add boilerplats

---
+++ lib/DDG/Longtail/Wikipedia/BoilerPlate.pm
@@ -0,0 +1,14 @@
+package DDG::Longtail::AndroidEnthusiasts;
+
+use DDG::Meta::Information;
+use DDG::Longtail;
+
+name 'AndroidEnthusiasts';
+description 'Android answers';
+source "Android Enthusiasts";
+icon_url "/i/android.stackexchange.com.ico";
+topics => 'geek';
+categories => 'q/a';
+primary_example_queries => 'android itunes';
+secondary_example_queries => 'android apk sd card', 'android root password';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/AndroidEnthusiasts/';
