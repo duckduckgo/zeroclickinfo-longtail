c67f683af434fff8fbdb831ccdb5a7207307775c
yegg <yegg@duckduckgo.com>
2012-11-06 14:24:21 -0500

    Add AskUbuntu

---
+++ lib/DDG/Longtail/AskUbuntu/AskUbuntu.pm
@@ -0,0 +1,14 @@
+package DDG::Longtail::AskUbuntu;
+
+use DDG::Meta::Information;
+use DDG::Longtail;
+
+name 'AskUbuntu';
+description 'Ubuntu answers';
+source "Ubuntu StackExchange";
+icon_url "/i/askubuntu.com.ico";
+topics => 'sysadmin';
+categories => 'q/a';
+primary_example_queries => 'battery life ubuntu kubuntu';
+secondary_example_queries => 'gedit split pane', 'reset keyboard layout';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/AskUbuntu/';
