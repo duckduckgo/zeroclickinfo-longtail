b79ed3dcbc5a00104b5f3c3109e1d58f99174b0a
yegg <yegg@duckduckgo.com>
2012-11-06 15:01:50 -0500

    Add Drupal

---
+++ lib/DDG/Longtail/Drupal/Drupal.pm
@@ -0,0 +1,14 @@
+package DDG::Longtail::Drupal;
+
+use DDG::Meta::Information;
+use DDG::Longtail;
+
+name 'Drupal';
+description 'Drupal answers';
+source "Drupal StackExchange";
+icon_url "/i/drupal.stackexchange.com.ico";
+topics => 'web_design';
+categories => 'q/a';
+primary_example_queries => 'convert wordpress to drupal theme';
+secondary_example_queries => 'hook_preprocess_page', 'drupal_goto';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/Drupal/';
