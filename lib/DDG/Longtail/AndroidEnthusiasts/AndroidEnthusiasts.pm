20e9f2cfec13e1e1982715a875724e3c3ea8e352
yegg <yegg@duckduckgo.com>
2012-11-06 13:26:32 -0500

    Android Enthusiasts stub to test new format.

---
+++ lib/DDG/Longtail/AndroidEnthusiasts/AndroidEnthusiasts.pm
@@ -0,0 +1,12 @@
+package DDG::Goodie::AndroidEnthusiasts;
+
+use DDG::Meta::Information;
+use DDG::Longtail;
+
+primary_example_queries => '5 letter words';
+secondary_example_queries => '12 character word';
+description 'Android answers';
+name 'AndroidEnthusiasts';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/AndroidEnthusiasts.pm';
+topics => 'geek';
+categories => 'q/a';
