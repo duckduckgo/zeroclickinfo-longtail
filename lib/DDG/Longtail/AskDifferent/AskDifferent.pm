2ee3f7eb2bccd4f8107c747499aac233ba5a153b
yegg <yegg@duckduckgo.com>
2012-11-06 14:14:46 -0500

    Add AskDifferent

---
+++ lib/DDG/Longtail/AskDifferent/AskDifferent.pm
@@ -0,0 +1,14 @@
+package DDG::Longtail::AskDifferent;
+
+use DDG::Meta::Information;
+use DDG::Longtail;
+
+name 'AskDifferent';
+description 'Apple answers';
+source "Apple StackExchange";
+icon_url "/i/apple.stackexchange.com.ico";
+topics => 'geek';
+categories => 'q/a';
+primary_example_queries => 'ipod touch gps';
+secondary_example_queries => 'mac os x keyboard shortcuts', 'change os x theme';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/AskDifferent/';
