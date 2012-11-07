ffdc6a7f2c250f00a537835b4c52db1db5b2b46a
moollaza <moollaza@gmail.com>
2012-11-07 14:50:49 -0500

    fixed longtail meta information 's/categories/category' and 's/^([a-z_]+) =>/\1/'

--- lib/DDG/Longtail/Tex/Tex.pm
+++ lib/DDG/Longtail/Tex/Tex.pm
@@ -7,8 +7,8 @@ name 'Tex';
 description 'Typesetting answers';
 source "Latex StackExchange"
 icon_url "/i/tex.stackexchange.com.ico";
-topics => 'special_interest';
-categories => 'q/a';
-primary_example_queries => 'matrix latex document';
-secondary_example_queries => 'define macros lyx', 'resume template latex';
+topics 'special_interest';
+category 'q/a';
+primary_example_queries 'matrix latex document';
+secondary_example_queries 'define macros lyx', 'resume template latex';
 code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/Tex/';
--- lib/DDG/Longtail/Wikipedia/Wikipedia.pm
+++ lib/DDG/Longtail/Wikipedia/Wikipedia.pm
@@ -7,8 +7,8 @@ name 'Wikipedia';
 description 'Wikipedia full text search';
 source "Wikipedia";
 icon_url "/i/wikipedia.com.ico"
-topics => 'everyday';
-categories => 'q/a';
-primary_example_queries => 'snow albedo';
-secondary_example_queries => 'darpa early history', 'will mcraven';
+topics 'everyday';
+category 'q/a';
+primary_example_queries 'snow albedo';
+secondary_example_queries 'darpa early history', 'will mcraven';
 code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/Wikipedia/';
