2b02add28625bee1c842d938a4945c0afc4ee855
Caine Tighe <caine@duckduckgo.com>
2012-11-07 11:47:55 -0500

    StackOverflow

---
+++ lib/DDG/Longtail/StackOverflow/StackOverflow.pm
@@ -0,0 +1,14 @@
+package DDG::Longtail::StackOverflow;
+
+use DDG::Meta::Information;
+use DDG::Longtail;
+
+name 'StackOverflow';
+description 'Programming answers';
+source "Stack Overflow";
+icon_url "/i/www.stackoverflow.com.ico";
+topics => 'programming';
+categories => 'q/a';
+primary_example_queries => 'apache nginx';
+secondary_example_queries => 'sorting numbers in ram', 'BerkeleyDB Concurrency';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/StackOverflow/';
