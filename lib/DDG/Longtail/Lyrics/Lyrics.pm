8f34e9cb557569fb84e51e0ac8d51e87c8258609
Caine Tighe <caine@duckduckgo.com>
2012-11-07 10:47:21 -0500

    Add Lyrics

---
+++ lib/DDG/Longtail/Lyrics/Lyrics.pm
@@ -0,0 +1,14 @@
+package DDG::Longtail::Lyrics;
+
+use DDG::Meta::Information;
+use DDG::Longtail;
+
+name 'Lyrics';
+description 'Lyrics';
+source "LyricsMode";
+icon_url "/i/www.lyricsmode.com.ico";
+topics => 'music';
+categories => 'q/a';
+primary_example_queries => 'brass monkey lyrics';
+secondary_example_queries => 'panama lyrics', 'lean on me lyrics';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/Lyrics/';
