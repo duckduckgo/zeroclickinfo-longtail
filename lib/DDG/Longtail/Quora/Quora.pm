23edff57798e43981f45386ce0cd00309727c2a9
Caine Tighe <caine@duckduckgo.com>
2012-11-07 11:19:52 -0500

    Add Quora (not live yet)

---
+++ lib/DDG/Longtail/Quora/Quora.pm
@@ -0,0 +1,14 @@
+package DDG::Longtail::Quora;
+
+use DDG::Meta::Information;
+use DDG::Longtail;
+
+name 'Quora';
+description 'Quora';
+source "Quora";
+icon_url "/i/quora.com.ico";
+topics => 'trivia';
+categories => 'q/a';
+primary_example_queries => '';
+secondary_example_queries => '', '';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/Quora/';
