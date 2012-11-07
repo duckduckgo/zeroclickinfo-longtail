3ae160c495e297043d18aa730bf0f5371e8a6421
Caine Tighe <caine@duckduckgo.com>
2012-11-07 12:15:25 -0500

    Add WebApplications

---
+++ lib/DDG/Longtail/WebApplications/WebApplications.pm
@@ -0,0 +1,14 @@
+package DDG::Longtail::WebApplications;
+
+use DDG::Meta::Information;
+use DDG::Longtail;
+
+name 'WebApplications';
+description 'Web application discussion';
+source "WebApplication StackExchange";
+icon_url "/i/webapps.stackexchange.com.ico";
+topics => 'programming';
+categories => 'q/a';
+primary_example_queries => 'delete my facebook account';
+secondary_example_queries => 'facebook detects gmail', 'forward multiple emails gmail';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/WebApplications/';
