3163cd9837f02a1be1ef37808b1c03d8416ec6ae
Caine Tighe <caine@duckduckgo.com>
2012-11-07 11:45:03 -0500

    Skeptics SE

---
+++ lib/DDG/Longtail/Skeptics/Skeptics.pm
@@ -0,0 +1,14 @@
+package DDG::Longtail::Skeptics;
+
+use DDG::Meta::Information;
+use DDG::Longtail;
+
+name 'Skeptics';
+description 'Scientific Skeptics';
+source "Skeptics StackExchange";
+icon_url "/i/skeptics.stackexchange.com.ico";
+topics => 'science';
+categories => 'q/a';
+primary_example_queries => 'mix lysol and bleach';
+secondary_example_queries => 'more babies during full moons', 'adult multi-vitamin';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/Skeptics/';
