c1c6368bf2acb5abdc522e06a0fe1af57636bbd8
Caine Tighe <caine@duckduckgo.com>
2012-11-07 12:02:21 -0500

    CS Theory

---
+++ lib/DDG/Longtail/TheoreticalComputerScience/TheoreticalComputerScience.pm
@@ -0,0 +1,14 @@
+package DDG::Longtail::TheoreticalComputerScience;
+
+use DDG::Meta::Information;
+use DDG::Longtail;
+
+name 'TheoreticalComputerScience';
+description 'Computer science theory';
+source "CS Theory StackExchange";
+icon_url "/i/cstheory.stackexchange.com.ico";
+topics => 'science';
+categories => 'q/a';
+primary_example_queries => 'generalizing the fft';
+secondary_example_queries => 'DFA intersection', 'error-correcting theory';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/TheoreticalComputerScience/';
