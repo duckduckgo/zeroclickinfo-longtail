ccdb9ceb74fa3faa84b6504bce91d59e62fabe17
Caine Tighe <caine@duckduckgo.com>
2012-11-07 12:10:46 -0500

    Add UX

---
+++ lib/DDG/Longtail/UserExperience/UserExperience.pm
@@ -0,0 +1,14 @@
+package DDG::Longtail::UserExperience;
+
+use DDG::Meta::Information;
+use DDG::Longtail;
+
+name 'UserExperience';
+description 'User experience discussion';
+source "UserExperience StackExchange";
+icon_url "/i/ux.stackexchange.com.ico";
+topics => 'special_interest';
+categories => 'q/a';
+primary_example_queries => 'limit undo history';
+secondary_example_queries => 'inspire users to strong password', 'view deep hierarchy';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/UserExperience/';
