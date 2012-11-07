708a8ea66fcd84342831445e017bc5a6a78f4c70
Caine Tighe <caine@duckduckgo.com>
2012-11-07 11:10:16 -0500

    Add Physics

---
+++ lib/DDG/Longtail/Physics/Physics.pm
@@ -0,0 +1,14 @@
+package DDG::Longtail::Physics;
+
+use DDG::Meta::Information;
+use DDG::Longtail;
+
+name 'Physics';
+description 'Physics answers';
+source "Physics StackExchange";
+icon_url "/i/physics.stackexchange.com.ico";
+topics => 'science';
+categories => 'q/a';
+primary_example_queries => 'photons per second in lumen';
+secondary_example_queries => 'sound propagation air pressure', 'is the ISS electrically charged';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/Physics/';
