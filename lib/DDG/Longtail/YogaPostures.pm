e88b18af34d9f41efc4fb07be5058873cb68bd60
phwaap <thistle@withoutopus.org>
2014-12-10 15:08:46 -0700

    * Initial commit

---
+++ lib/DDG/Longtail/YogaPostures.pm
@@ -0,0 +1,17 @@
+package DDG::Longtail::YogaPostures;
+
+use DDG::Longtail;
+
+primary_example_queries 'Downward dog pose', 'marichyasana a';
+secondary_example_queries 'fuel economy 2012 Nissan Leaf', 'mpg 2011 Infiniti M37x';
+description "Yoga postures with English translation";
+name "Yoga Postures";
+icon_url 'http://www.fueleconomy.gov/favicon.ico';
+source 'FuelEconomy.gov';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/tree/master/share/longtail/yoga_postures';
+topics 'special_interest';
+category 'reference';
+attribution
+    github => ['zachthompson', 'Zach Thompson'];
+
+1;
