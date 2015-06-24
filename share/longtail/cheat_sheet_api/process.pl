6445143e396f8956bfd20fa13de9bae4ad7c7ab4
Zach Thompson <zach@duckduckgo.com>
2015-06-24 16:18:34 -0600

    * Use paragraph for cheat sheet; fix typos with filehandle and source attribute

--- share/longtail/cheat_sheet_api/process.pl
+++ share/longtail/cheat_sheet_api/process.pl
@@ -9,27 +9,28 @@ my $output_file = 'output.xml';
 my $additional_keywords = join(' ', ' char', 'character', 'cheat sheet', 'cheatsheet', 'command', 'example', 'guide', 'help', 'quick reference', 'shortcut', 'symbol');
 
 MAIN:{
-	open my $output, '>:utf8', $output_file or die "Failed to open $output_file: $!";
+    open my $output, '>:utf8', $output_file or die "Failed to open $output_file: $!";
 
-	print $output qq|<?xml version="1.0" encoding="UTF-8"?>\n<add allowDups="true">|;
-	while(my $f = <json/*.json>){
-	    my $j = read_file($f, binmode => ':utf8');
-	    my $cs = from_json($j);
-		my $keywords = delete $cs->{keywords};
-		die "No keywords specified in $f" unless $keywords;
-		$keywords .= $additional_keywords;
-		my %k; # de-dupe and normalize
-		for my $kw (split /\s+/, $keywords){
-			++$k{$kw};
-		}
-		$keywords = join(' ', keys %k);
+    print $output qq|<?xml version="1.0" encoding="UTF-8"?>\n<add allowDups="true">|;
+    while(my $f = <json/*.json>){
+        my $j = read_file($f, binmode => ':utf8');
+        my $cs = from_json($j);
+        my $keywords = delete $cs->{keywords};
+        die "No keywords specified in $f" unless $keywords;
+        $keywords .= $additional_keywords;
+        my %k; # de-dupe and normalize
+        for my $kw (split /\s+/, $keywords){
+            ++$k{$kw};
+        }
+        $keywords = join(' ', keys %k);
 
-		print $output "\n", join("\n",
+        print$output "\n", join("\n",
             qq{<doc>},
-            qq{<field name="title"><![CDATA[$cs->{name}]]></field>},
+            qq|<field name="title"><![CDATA[$cs->{name}]]></field>|,
             qq{<field name="l2_sec_match2"><![CDATA[$keywords]]></field>},
-            q|<field name="meta"><![CDATA[{"cheat_sheet":"| . to_json($cs) . q|"}]]></field>|,
-            qq{</doc>});
-	}
-	print output "\n</add>";
+            q|<field name="paragraph"><![CDATA[| . to_json($cs) . q|$pp]]></field>|,
+            '<field name="source"><![CDATA[cheat_sheet_api]]></field>');
+            
+    }
+    print $output "\n</add>";
 }
