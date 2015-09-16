19597f041e50bea670305ac43a0e061c85b6e9ce
Zach Thompson <zach@duckduckgo.com>
2015-09-16 08:44:51 -0600

    Add templates; remove additional keywords

--- share/longtail/cheat_sheet_api/process.pl
+++ share/longtail/cheat_sheet_api/process.pl
@@ -7,7 +7,7 @@ my $output_file = 'output.json';
 my $pretty_json = 1;
 
 # provide default keywords for each cheat sheet
-my $additional_keywords = join(' ', ' char', 'character', 'cheat sheet', 'cheatsheet', 'command', 'example', 'guide', 'help', 'quick reference', 'shortcut', 'symbol');
+#my $additional_keywords = join(' ', ' char', 'character', 'cheat sheet', 'cheatsheet', 'command', 'example', 'guide', 'help', 'quick reference', 'shortcut', 'symbol');
 
 MAIN:{
 
@@ -17,18 +17,34 @@ MAIN:{
         my $cs = from_json($j);
         my $keywords = delete $cs->{keywords};
         die "No keywords specified in $f" unless $keywords;
-        $keywords .= $additional_keywords;
-        my %k; # de-dupe and normalize
-        for my $kw (split /\s+/, $keywords){
-            ++$k{$kw};
-        }
-        $keywords = join(' ', keys %k);
+        #$keywords .= $additional_keywords;
+        #my %k; # de-dupe and normalize
+        #for my $kw (split /\s+/, $keywords){
+        #    ++$k{$kw};
+        #}
+        #$keywords = join(' ', keys %k);
+		my %text_fields;
+		my $i = 0;
+		while(my ($h, $s) = each %{$cs->{sections}}){
+			for my $cheat (@$s){
+				my ($k, $v) = @$cheat{qw(key value)};
+				if($k){
+					$text_fields{++$i . '_text'} = $k;
+					$text_fields{++$i . '_text_punctuation_removed'} = $k;
+				}
+				if($v){
+					$text_fields{++$i . '_text'} = $v;
+					$text_fields{++$i . '_text_punctuation_removed'} = $v;
+				}
+			}
+		}
 
 		push @jdocs, {
 			title => $cs->{name},
 			l2_sec_match2 => $keywords,
-			paragraph => to_json($cs),
-			source => 'cheat_sheet_api'
+			%text_fields,
+			source => 'cheat_sheet_api',
+			meta => to_json({cs => $cs})
 		};
     }
     open my $output, '>:utf8', $output_file or die "Failed to open $output_file: $!";
