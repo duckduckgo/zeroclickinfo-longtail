d49c7700bfd5ba72dee07528929daf9f24632765
bfmags <bfmags@gmail.com>
2016-07-13 22:59:02 +0000

    py script to parse output to xml

---
+++ lib/longtail/mdnjs/toxml.py
@@ -0,0 +1,35 @@
+"""
+ Generate a longtail XML dump of the Fathead output.txt
+"""
+import csv
+
+from parse import FatWriter
+
+XML = """<?xml version="1.0" encoding="UTF-8"?>
+<docs>
+    {results}
+</docs>
+"""
+
+DOC = """<doc>
+	<field name="title"><![CDATA[{title}]]></field>
+	<field name="l2_sec"><![CDATA[{abstract}]]></field>
+	<field name="paragraph"><![CDATA[{abstract} <a href="{source_url}">{source_url}</a>]]></field>
+	<field name="p_count">1</field>
+	<field name="source"><![CDATA[instant_answer_id]]></field>
+</doc>"""
+
+def run(infname, outfname):
+    infile = open(infname)
+    reader = csv.DictReader(infile, FatWriter.FIELDS, dialect='excel-tab')
+    with open(outfname, 'w') as outfile:
+        rows = []
+        for line in reader:
+            rows.append(DOC.format(**line))
+        results = '\n'.join(rows)
+        outfile.write(XML.format(results=results).replace('\\n', '\n'))
+
+if __name__ == '__main__':
+    infname = 'output.txt'
+    outfname = 'output.xml'
+    run(infname, outfname)
